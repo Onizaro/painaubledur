f7fd1f6c2df032b292a39591940144d1055eed07
unknown <nizaroumehdi@gmail.com>
2024-03-19 16:32:40 +0100

    ajout des liens avec animations"

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 
-function spancreation() {
-    const title = document.querySelector('h1');
+function spancreation(selector, classe) {
+    const title = document.getElementById(selector);
     let letters = title.textContent.split('');
     title.innerHTML= '';
     for (let i = 0 ; i < letters.length; i++){
         
-        title.innerHTML += '<span class="letter">'+letters[i]+'</span>';
+        title.innerHTML += '<span class="'+classe+'">'+letters[i]+'</span>';
     }
 
     return 0;
-
 }
 
-spancreation();
+spancreation('title', 'letter');
